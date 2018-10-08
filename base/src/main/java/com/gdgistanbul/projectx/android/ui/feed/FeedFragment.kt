3ef9d3226c231a2f192af5e8402c0c6b7338dc7d
Elif Boncuk <elifboncuk88@gmail.com>
2018-10-09 00:01:10 +0300

    Add Recycler View

--- base/src/main/java/com/gdgistanbul/projectx/android/ui/feed/FeedFragment.kt
+++ base/src/main/java/com/gdgistanbul/projectx/android/ui/feed/FeedFragment.kt
@@ -4,7 +4,9 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
+import android.databinding.BindingAdapter
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,6 +15,7 @@ import com.gdgistanbul.projectx.android.BaseNavigationFragment
 import dagger.android.support.DaggerFragment
 import javax.inject.Inject
 import com.gdgistanbul.projectx.android.base.databinding.FragmentFeedBinding;
+import com.gdgistanbul.projectx.android.data.feed.model.response.FeedItem
 import dagger.android.support.AndroidSupportInjection
 
 
@@ -20,21 +23,26 @@ class FeedFragment : BaseFragment(), BaseNavigationFragment {
 
 
     private lateinit var feedContentViewModel: FeedContentViewModel
+    private lateinit var binding: FragmentFeedBinding
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        feedContentViewModel = ViewModelProviders.of(this, viewModelProviderFactory).get(FeedContentViewModel::class.java)
 
-        val binding = FragmentFeedBinding.inflate(inflater, container, false).apply {
+        binding = FragmentFeedBinding.inflate(inflater, container, false).apply {
             setLifecycleOwner(this@FeedFragment)
-            viewModel = this@FeedFragment.feedContentViewModel
         }
 
+       return binding.root
+    }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        feedContentViewModel = ViewModelProviders.of(this, viewModelProviderFactory).get(FeedContentViewModel::class.java)
 
+        binding.viewModel = feedContentViewModel
 
         feedContentViewModel.fetchFeedContent()
 
@@ -42,9 +50,6 @@ class FeedFragment : BaseFragment(), BaseNavigationFragment {
         feedContentViewModel.feedContentLive.observe(this, Observer {
             // viewstate.
         })
-
-
-       return binding.root
     }
 
 }
